fc7550f191a17d7eba7dbd9b8c197e6cb0677067
DeveloperMatheus97 <matheuscaitanosouza18@outlook.com>
2022-05-05 11:13:27 -0300

    first commit

---
+++ Ex06/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, delta, r1, r2;
+            string[] vet = Console.ReadLine().Split(' ');
+
+            a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            delta = Math.Pow(b, 2.0) - 4 * a * c;
+
+            if (a == 0 || delta < 0.0)
+            {
+                Console.WriteLine("IMPOSSIVEL CALCULAR!!");
+            }
+            else
+            {
+                r1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+                r2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+                Console.WriteLine("R1 = " + r1.ToString("F5", CultureInfo.InvariantCulture));
+                Console.WriteLine("R2 = " + r2.ToString("F5", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
